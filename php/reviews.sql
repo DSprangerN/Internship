f3ccfa61b3facf997e0de73615855d6c000abd8c
DavidSNeves <148289372+Spingamii@users.noreply.github.com>
2025-05-22 19:56:43 +0100

    reviews

--- php/reviews.sql
+++ php/reviews.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.4.3 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.8.0.6908
+-- Anfitrião:                    127.0.0.1
+-- Versão do servidor:           8.0.39 - MySQL Community Server - GPL
+-- SO do servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -14,7 +14,21 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+
+-- A despejar estrutura da base de dados para estrelinha_login
+CREATE DATABASE IF NOT EXISTS `estrelinha_login` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `estrelinha_login`;
+
+-- A despejar estrutura para tabela estrelinha_login.reviews
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `id_review` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `review` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_review`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- A despejar dados para tabela estrelinha_login.reviews: ~0 rows (aproximadamente)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
